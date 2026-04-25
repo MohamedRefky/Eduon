a4f44757f5536f3f4731c5cf43eac4ad6493e768
MohamedRefky <https://github.com/MohamedRefky/TaskiApp.git>
2026-04-25 17:12:27 +0300

    feat(ai_service): update API key and model configuration for AI service

--- lib/core/service/ai_service.dart
+++ lib/core/service/ai_service.dart
@@ -2,8 +2,10 @@ import 'package:dio/dio.dart';
 
 class AiService {
   final String _apiKey =
-      "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";
-  final String _model = "arcee-ai/trinity-large-preview:free";
+      "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";
+  //final String _model = "arcee-ai/trinity-large-preview:free";
+  //final String _model = "nvidia/nemotron-3-super-120b-a12b:free";
+  final String _model = "inclusionai/ling-2.6-flash:free";
 
   late final Dio _dio;
 
